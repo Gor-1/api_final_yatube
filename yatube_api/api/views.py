535899fae0a2fcde3944d2ef42ee3de144f2cefb
Gor Galstyan <gorqwertyuiop@mail.ru>
2023-02-08 17:08:03 +0300

    5

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -29,11 +29,11 @@ class CommentViewSet(viewsets.ModelViewSet):
         return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def get_queryset(self):
-        post = self.get_post(self)
+        post = self.get_post()
         return post.comments.all()
 
     def perform_create(self, serializer):
-        post = self.get_post(self)
+        post = self.get_post()
         serializer.save(author=self.request.user, post=post)
 
 
